bdd1ab7ce01ddd5b822fba4cf65898b6ee850540
Exceloids <lhorexcelbombarda22@gmail.com>
2023-10-02 10:38:32 +0800

    upload first php project

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=
+    
+    , initial-scale=1.0">
+    <title>Hi sir Prim! hehehe</title>
+</head>
+<body>
+    <h1>Hello back to zero!</h1>
+    <?php
+        echo 'testing';    
+    ?>
+
+</body>
+</html>
